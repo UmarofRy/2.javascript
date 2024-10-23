e37011e2ecdd1f50f757685086824b3d6ccd02f8
RyUmarof <ryumarof@gmail.com>
2024-10-23 16:22:52 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let a = prompt('string typedagi malumot kiriting')
+
+let b = prompt('Number typedagi malumot kiriting')
+
+let c = prompt('boolean typedagi malumot kiriting')
+
+confirm('hamma malumotlarni togri kiritganingizga ishonchingiz komilmi')
+
+alert(`
+         siz kiritgan  ${a}  string typega tegishli
+         siz kiritgan  ${b}  number typega tegishli
+         siz kiritgan  ${c}  boolean typega tegishli
+         `)
